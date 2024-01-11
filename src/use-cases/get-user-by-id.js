6cef6c77d0b3b7e1d44e89f2bc73108cd6cd485f
Tiago Alves <tiagonewalves@hotmail.com>
2024-01-11 15:26:05 -0300

    feat: add GetUserByIdController and validations

--- src/use-cases/get-user-by-id.js
+++ src/use-cases/get-user-by-id.js
@@ -1,6 +1,6 @@
 import { PostGresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id.js'
 
-export class GetUserbyIdUseCase {
+export class GetUserByIdUseCase {
     async execute(userId) {
         const getUserByIdRepository = new PostGresGetUserByIdRepository()
         const user = await getUserByIdRepository.execute(userId)
