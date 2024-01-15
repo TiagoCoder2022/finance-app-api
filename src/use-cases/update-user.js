652f287a0a644ead198828b4bf5fe254c37ff291
Tiago Alves <tiagonewalves@hotmail.com>
2024-01-15 13:20:51 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt'
+import { PostgreGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUsecase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgreGetUserByEmailRepository()
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}
