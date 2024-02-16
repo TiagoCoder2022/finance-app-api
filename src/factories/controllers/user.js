855c79a3e2abb04d8aa07985b1de90db570f916c
Tiago Alves <tiagonewalves@hotmail.com>
2024-02-16 12:56:00 -0300

    feat: add user controllers factories

---
+++ src/factories/controllers/user.js
@@ -0,0 +1,68 @@
+import {
+    GetUserByIdController,
+    CreateUserController,
+    UpdateUserController,
+    DeleteUserController,
+} from '../../controllers/index.js'
+import {
+    PostGresGetUserByIdRepository,
+    PostgresCreateUserRepository,
+    PostgreGetUserByEmailRepository,
+    PostgresUpdateUserRepository,
+    PostgresDeleteUserRepository,
+} from '../../repositories/postgres/index.js'
+import {
+    GetUserByIdUseCase,
+    CreateUserUseCase,
+    UpdateUserUsecase,
+    DeleteUserUseCase,
+} from '../../use-cases/index.js'
+
+export const makeGetUserController = () => {
+    const getUserByIdRepository = new PostGresGetUserByIdRepository()
+
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository)
+
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase)
+
+    return getUserByIdController
+}
+
+export const makeCreateUserController = () => {
+    const getUserByEmailRepository = new PostgreGetUserByEmailRepository()
+    const createUserRepository = new PostgresCreateUserRepository()
+
+    const createUserUseCase = new CreateUserUseCase(
+        getUserByEmailRepository,
+        createUserRepository,
+    )
+
+    const createUserController = new CreateUserController(createUserUseCase)
+
+    return createUserController
+}
+
+export const makeUpdateUserController = () => {
+    const getUserByEmailRepository = new PostgreGetUserByEmailRepository()
+
+    const updateUserRepository = new PostgresUpdateUserRepository()
+
+    const updateUserUseCase = new UpdateUserUsecase(
+        getUserByEmailRepository,
+        updateUserRepository,
+    )
+
+    const updateUserController = new UpdateUserController(updateUserUseCase)
+
+    return updateUserController
+}
+
+export const makeDeleteUserController = () => {
+    const deleteUserRepository = new PostgresDeleteUserRepository()
+
+    const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository)
+
+    const deleteUserController = new DeleteUserController(deleteUserUseCase)
+
+    return deleteUserController
+}
