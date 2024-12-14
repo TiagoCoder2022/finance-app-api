6616c388667d4a397269a02fda5a9ba32e0d6332
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-13 22:20:56 -0300

    feat: add UpdateTransactionController

--- src/controllers/transaction/update-transaction.js
+++ src/controllers/transaction/update-transaction.js
@@ -1,20 +1,66 @@
-import { UserNotFoundError } from '../../errors/user.js'
-export class UpdateTransactionUseCase {
-    constructor(updateTransactionRepository, getUserByIdRepository) {
-        this.updateTransactionRepository = updateTransactionRepository
-        this.getUserByIdRepository = getUserByIdRepository
+import {
+    badRequest,
+    checkIfAmountIsValid,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidAmountResponse,
+    invalidIdResponse,
+    invalidTypeResponse,
+    ok,
+    serverError,
+} from '../helpers/index.js'
+export class UpdateTransactionController {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase
     }
 
-    async execute(params) {
-        const user = await this.getUserByIdRepository.execute(params.userId)
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
 
-        if (!user) {
-            throw new UserNotFoundError()
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            const allowedFields = ['name', 'date', 'amount', 'type']
+
+            const someFieldIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            )
+
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed.',
+                })
+            }
+
+            if (params.amount) {
+                const amountIsValid = checkIfAmountIsValid(params.amount)
+
+                if (!amountIsValid) {
+                    return invalidAmountResponse()
+                }
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type)
+
+                if (!typeIsValid) {
+                    return invalidTypeResponse()
+                }
+            }
+
+            const transaction = await this.updateTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+                params,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            console.error(error)
+            return serverError()
         }
-
-        const transaction =
-            await this.updateTransactionRepository.execute(params)
-
-        return transaction
     }
 }
