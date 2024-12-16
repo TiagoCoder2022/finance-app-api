8be082e7f6ee1289a8d39db9f78882ef12de9bd1
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-15 21:39:14 -0300

    feat: add zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,15 +1,7 @@
-import {
-    invalidPasswordResponde,
-    emailIsAlreadyInUseResponse,
-    checkIfPasswordIsValid,
-    checkIfEmailIsValid,
-    badRequest,
-    created,
-    serverError,
-    validateRequiredFields,
-    requiredFieldIsMissingResponse,
-} from '../helpers/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
 import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { ZodError } from 'zod'
+import { createUserSchema } from '../../schemas/user.js'
 
 export class CreateUserController {
     constructor(createUserUseCase) {
@@ -20,42 +12,17 @@ export class CreateUserController {
         try {
             const params = httpRequest.body
 
-            const requiredFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
-
-            const { ok: requiredFieldsWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields)
-
-            if (!requiredFieldsWereProvided) {
-                return requiredFieldIsMissingResponse(missingField)
-            }
-
-            for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
-                    return badRequest({ message: `Missing param: ${field}` })
-                }
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponde()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return emailIsAlreadyInUseResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             const createdUser = await this.createUserUseCase.execute(params)
 
             return created(createdUser)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({
                     message: error.message,
