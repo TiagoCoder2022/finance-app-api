ed154f48205ada63b17b4a2d5f408dc3d2cb2fec
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-19 11:34:28 -0300

    test: ensure createUserUseCase is called with correct params

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -22,7 +22,7 @@ describe('Create User Controller', () => {
         const result = await createUserController.execute(httpRequest)
 
         expect(result.statusCode).toBe(201)
-        expect(result.body).toBe(httpRequest.body)
+        expect(result.body).toEqual(httpRequest.body)
     })
 
     it('should return 400 if first_name is not passed.', async () => {
@@ -121,5 +121,24 @@ describe('Create User Controller', () => {
         const result = await createUserController.execute(httpRequest)
 
         expect(result.statusCode).toBe(400)
+    })
+
+    it('should call CreateUserUseCse with correct params', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Tiago',
+                last_name: 'Alves',
+                email: 'tiago@email.com',
+                password: '1234567',
+            },
+        }
+
+        const executeSpy = jest.spyOn(createUserUseCase, 'execute')
+
+        await createUserController.execute(httpRequest)
+
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
     })
 })
