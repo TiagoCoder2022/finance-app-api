ad5cc5c1cc3bb297efa9f7481a9261ae91e48384
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-19 10:32:50 -0300

    test: ensure 400 is returned when email and password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -56,4 +56,36 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided.', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Tiago',
+                last_name: 'Alves',
+                password: '1234567',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if password is not provided.', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Tiago',
+                last_name: 'Alves',
+                email: 'tiago@email.com',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })
