5c17749e51990c3a96cdce2a732601dbe6207a43
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-19 10:46:22 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -73,6 +73,23 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(400)
     })
 
+    it('should return 400 if email is not valid.', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Tiago',
+                last_name: 'Alves',
+                email: 'tiagoemail.com',
+                password: '1234567',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
+
     it('should return 400 if password is not provided.', async () => {
         const createUserUseCase = new CreateUserUseCaseStub()
         const createUserController = new CreateUserController(createUserUseCase)
