573f71719d193bc79834c476fa789f2ada04af0e
Tiago Alves <tiagonewalves@hotmail.com>
2024-12-19 09:15:09 -0300

    test: ensure $00 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -24,4 +24,20 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
     })
+
+    it('should return 400 if first_name is not passed.', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                last_name: 'Alves',
+                email: 'pam@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })
