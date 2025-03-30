6b537b44f75ae7166857a54a45baa6f3f30f60f0
Tiago Alves <tiagonewalves@hotmail.com>
2025-03-29 21:25:57 -0300

    refactor: ensure GetUserBalanceUseCase receives user id param

--- src/controllers/user/get-user-balance.js
+++ src/controllers/user/get-user-balance.js
@@ -26,7 +26,7 @@ export class GetUserBalanceController {
                 return invalidIdResponse()
             }
 
-            const balance = await this.getUserBalanceUseCase.execute({ userId })
+            const balance = await this.getUserBalanceUseCase.execute(userId)
 
             return ok(balance)
         } catch (error) {
