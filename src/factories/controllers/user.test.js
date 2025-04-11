4a881e7b8600b8140c0106460f11e616e638787e
Tiago Alves <tiagonewalves@hotmail.com>
2025-04-11 11:42:16 -0300

    test: ensure makeCreateUserController returns valid CreateUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,10 +1,14 @@
-import { GetUserByIdController } from '../../controllers'
-import { makeGetUserByIdController } from './user'
+import { CreateUserController, GetUserByIdController } from '../../controllers'
+import { makeCreateUserController, makeGetUserByIdController } from './user'
 
 describe('User Controller Factories', () => {
     it('should return a valid GetUserByIdController instance', async () => {
         expect(makeGetUserByIdController()).toBeInstanceOf(
             GetUserByIdController,
         )
+    })
+
+    it('should return a valid CreateUserController instance', async () => {
+        expect(makeCreateUserController()).toBeInstanceOf(CreateUserController)
     })
 })
