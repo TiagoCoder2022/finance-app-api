671b2f345d0c6b459f13585ed21a73f3b0739499
Tiago Alves <tiagonewalves@hotmail.com>
2025-04-14 19:01:51 -0300

    style: fix code style issues with Prettier

--- src/app.cjs
+++ src/app.cjs
@@ -12,7 +12,7 @@ app.use('/api/users', usersRouter)
 app.use('/api/transactions', transactionsRouter)
 
 const swaggerDocument = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8')
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8'),
 )
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
