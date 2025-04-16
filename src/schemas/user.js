b28793624d20a7a1eb10dcd22e78b017a5aaebe0
Tiago Alves <tiagonewalves@hotmail.com>
2025-04-16 19:04:53 -0300

    feat: add LoginUserController

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -40,3 +40,18 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema.partial().strict({
     message: 'Some provided field is not allowed.',
 })
+
+export const loginSchema = z.object({
+    email: z
+        .string()
+        .email({
+            message: 'Please provide a valid email.',
+        })
+        .trim()
+        .min(1, {
+            message: 'E-mail is required',
+        }),
+    password: z.string().trim().min(6, {
+        message: 'Password must have at least 6 characters.',
+    }),
+})
